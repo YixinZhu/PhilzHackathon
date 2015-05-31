f38d61544156888c839a19ff0e06c354caff30ba
Yixin Zhu <yixin@uber.com>
2015-05-30 18:15:55 -0700

    added rating functionality

--- app/src/main/java/com/yixinzhu/philzmobile/BaristaMatchActivity.java
+++ app/src/main/java/com/yixinzhu/philzmobile/BaristaMatchActivity.java
@@ -2,12 +2,15 @@ package com.yixinzhu.philzmobile;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.firebase.client.DataSnapshot;
@@ -24,13 +27,17 @@ public class BaristaMatchActivity extends Activity {
     @InjectView(R.id.baristaText) TextView mTextView;
     @InjectView(R.id.orderEditText) EditText mEditText;
     @InjectView(R.id.submitOrder) Button mButton;
+    @InjectView(R.id.ratingBar) RatingBar mRatingBar;
 
     private ProgressDialog mProgressDialog;
+    private Context mContext;
+    private boolean orderSubmited;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barista_match);
+        mContext = this;
         ButterKnife.inject(this);
 
         mProgressDialog = new ProgressDialog(this);
@@ -49,8 +56,19 @@ public class BaristaMatchActivity extends Activity {
         MainActivity.mfireBaseRef.child("orderRequest/pending").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot != null && dataSnapshot.getValue().equals(false)) {
-
+                if (dataSnapshot != null && dataSnapshot.getValue().equals(false) && orderSubmited) {
+                    mEditText.setVisibility(View.GONE);
+                    mRatingBar.setVisibility(View.VISIBLE);
+                    mTextView.setText("Please rate Megan!");
+                    mButton.setText("Submit Rating");
+                    mButton.setEnabled(true);
+                    mButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            mContext.startActivity(new Intent(BaristaMatchActivity.this, MainActivity.class));
+                            finish();
+                        }
+                    });
                 }
             }
 
@@ -65,6 +83,8 @@ public class BaristaMatchActivity extends Activity {
             public void onClick(View v) {
                 MainActivity.mfireBaseRef.child("orderRequest/order").setValue(mEditText.getText().toString());
                 MainActivity.mfireBaseRef.child("orderRequest/pending").setValue(true);
+                mButton.setEnabled(false);
+                orderSubmited = true;
             }
         });
     }
